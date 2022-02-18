f45997698a6dddf22bbcb450f05bfc8d830c4e23
Dmytro Iakovliev <dmytro.iakovliev@zodiacsystems.com>
2022-02-18 11:25:34 +0200

    [setup.wizard] Add cache parameters page

--- pygavc/setup/wizard.py
+++ pygavc/setup/wizard.py
@@ -2,25 +2,79 @@ import sys
 import json
 from PyQt5 import QtCore, QtWidgets
 
-from ..gavc.gavc_parameters import GavcClientBaseParamsHandler
+from ..gavc.gavc_parameters import GavcClientBaseParamsHandler, GavcClientParamsHandler
 from ..gavc.artifactory_client import ArtifactoryClient
 from ..gavc.client_cache import ClientCache
 from ..gavc.user_config import UserConfig
 
 ################################################################################
+class CacheParamsPage(QtWidgets.QWizardPage):
+    cachePathSelected = QtCore.pyqtSignal(str)
+
+    def __init__(self, params_handler):
+        QtWidgets.QWizardPage.__init__(self)
+        self.setTitle("Artifactory client cache parameters")
+
+        self.__complete         = True
+        self.__params_handler   = params_handler
+
+        current_cache_path      = self.__params_handler.get_param(GavcClientParamsHandler.CACHE_PATH_PARAM)
+
+        layout = QtWidgets.QGridLayout()
+        rowIndex = 0
+
+        cachePathLabel = QtWidgets.QLabel()
+        cachePathLabel.setText("Cache path")
+
+        cachePathEditor = QtWidgets.QLineEdit()
+        cachePathEditor.setText(current_cache_path)
+        cachePathEditor.textChanged.connect(self.on_cache_path_changed)
+        self.cachePathSelected.connect(cachePathEditor.setText)
+
+        cachePathSelectButton = QtWidgets.QPushButton()
+        cachePathSelectButton.setText("...")
+        cachePathSelectButton.clicked.connect(self.on_select_button_clicked)
+
+        layout.addWidget(cachePathLabel, rowIndex, 0)
+        layout.addWidget(cachePathEditor, rowIndex, 1)
+        layout.addWidget(cachePathSelectButton, rowIndex, 2)
+        rowIndex += 1
+
+        self.setLayout(layout)
+
+    def isComplete(self):
+        return self.__complete
+
+    def __reset_complete(self):
+        self.__complete = False
+        self.completeChanged.emit()
+
+    def on_cache_path_changed(self, cache_path_value):
+        self.__params_handler.set_param(GavcClientParamsHandler.CACHE_PATH_PARAM, cache_path_value)
+
+    def on_select_button_clicked(self):
+        current_cache_path      = self.__params_handler.get_param(GavcClientParamsHandler.CACHE_PATH_PARAM)
+
+        dialog = QtWidgets.QFileDialog(self)
+        dialog.setFileMode(QtWidgets.QFileDialog.Directory)
+        dialog.selectFile(current_cache_path)
+        if dialog.exec():
+            self.cachePathSelected.emit(dialog.selectedFiles()[0])
+
+################################################################################
 class ConnectionParamsPage(QtWidgets.QWizardPage):
     checkingComplete = QtCore.pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self, params_handler):
         QtWidgets.QWizardPage.__init__(self)
         self.setTitle("Artifactory connection parameters")
 
-        params_handler = GavcClientBaseParamsHandler()
+        self.__complete         = False
+        self.__params_handler   = params_handler
 
-        self.__complete     = False
-        self.__server       = params_handler.get_param(GavcClientBaseParamsHandler.SERVER_PARAM)
-        self.__repository   = params_handler.get_param(GavcClientBaseParamsHandler.REPOSITORY_PARAM)
-        self.__token        = params_handler.get_param(GavcClientBaseParamsHandler.TOKEN_PARAM)
+        current_server          = self.__params_handler.get_param(GavcClientBaseParamsHandler.SERVER_PARAM)
+        current_repository      = self.__params_handler.get_param(GavcClientBaseParamsHandler.REPOSITORY_PARAM)
+        current_token           = self.__params_handler.get_param(GavcClientBaseParamsHandler.TOKEN_PARAM)
 
         layout = QtWidgets.QGridLayout()
         rowIndex = 0
@@ -30,7 +84,7 @@ class ConnectionParamsPage(QtWidgets.QWizardPage):
         serverLabel.setText("Server url")
 
         serverEditor = QtWidgets.QLineEdit()
-        serverEditor.setText(self.__server)
+        serverEditor.setText(current_server)
         serverEditor.textChanged.connect(self.on_server_changed)
 
         layout.addWidget(serverLabel, rowIndex, 0)
@@ -42,7 +96,7 @@ class ConnectionParamsPage(QtWidgets.QWizardPage):
         tokenLabel.setText("API access token")
 
         tokenEditor = QtWidgets.QLineEdit()
-        tokenEditor.setText(self.__token)
+        tokenEditor.setText(current_token)
         tokenEditor.textChanged.connect(self.on_token_changed)
 
         layout.addWidget(tokenLabel, rowIndex, 0)
@@ -54,7 +108,7 @@ class ConnectionParamsPage(QtWidgets.QWizardPage):
         repoLabel.setText("Repository")
 
         repoEditor = QtWidgets.QLineEdit()
-        repoEditor.setText(self.__repository)
+        repoEditor.setText(current_repository)
         repoEditor.textChanged.connect(self.on_repository_changed)
 
         layout.addWidget(repoLabel, rowIndex, 0)
@@ -76,29 +130,33 @@ class ConnectionParamsPage(QtWidgets.QWizardPage):
 
         self.setLayout(layout)
 
+    def isComplete(self):
+        return self.__complete
+
     def __reset_complete(self, text = "Not checked"):
         self.checkingComplete.emit(text)
         self.__complete = False
         self.completeChanged.emit()
 
     def on_token_changed(self, token_value):
-        self.__token = token_value
+        self.__params_handler.set_param(GavcClientBaseParamsHandler.TOKEN_PARAM, token_value)
         self.__reset_complete()
 
     def on_server_changed(self, server_value):
-        self.__server = server_value
+        self.__params_handler.set_param(GavcClientBaseParamsHandler.SERVER_PARAM, server_value)
         self.__reset_complete()
 
     def on_repository_changed(self, repository_value):
-        self.__repository = repository_value
+        self.__params_handler.set_param(GavcClientBaseParamsHandler.REPOSITORY_PARAM, repository_value)
+        self.__reset_complete()
 
     def check_connection(self):
-        cache   = ClientCache.from_params_handler()
-        client  = ArtifactoryClient(cache, self.__server, self.__repository, self.__token)
+        current_repository  = self.__params_handler.get_param(GavcClientBaseParamsHandler.REPOSITORY_PARAM)
+        client              = ArtifactoryClient.from_params_handler(self.__params_handler)
         try:
             r = client.requests().get_local_repos()
             repos_info = json.loads(r.text)
-            repo_info = list(filter(lambda x: x['key'] == self.__repository, repos_info))
+            repo_info = list(filter(lambda x: x['key'] == current_repository, repos_info))
             if len(repo_info) == 0:
                 raise Exception("No requested repository '%s'!" % self.__repository)
             self.checkingComplete.emit("OK")
@@ -108,18 +166,6 @@ class ConnectionParamsPage(QtWidgets.QWizardPage):
         except:
             self.__reset_complete("FAILED")
 
-    def isComplete(self):
-        return self.__complete
-
-    def server(self):
-        return self.__server
-
-    def repository(self):
-        return self.__repository
-
-    def token(self):
-        return self.__token
-
 ################################################################################
 class Wizard(QtWidgets.QWizard):
 
@@ -127,8 +173,12 @@ class Wizard(QtWidgets.QWizard):
         QtWidgets.QWizard.__init__(self)
         self.setWindowTitle("PyGAVC parameters")
 
-        self.__connection_params_page = ConnectionParamsPage()
+        self.__params_handler = GavcClientParamsHandler()
 
+        self.__cache_params_page        = CacheParamsPage(self.__params_handler)
+        self.__connection_params_page   = ConnectionParamsPage(self.__params_handler)
+
+        self.addPage(self.__cache_params_page)
         self.addPage(self.__connection_params_page)
 
         self.accepted.connect(self.on_accept)
@@ -136,10 +186,16 @@ class Wizard(QtWidgets.QWizard):
         self.finished.connect(self.on_finish)
 
     def on_accept(self):
+        current_cache_path      = self.__params_handler.get_param(GavcClientParamsHandler.CACHE_PATH_PARAM)
+        current_server          = self.__params_handler.get_param(GavcClientBaseParamsHandler.SERVER_PARAM)
+        current_repository      = self.__params_handler.get_param(GavcClientBaseParamsHandler.REPOSITORY_PARAM)
+        current_token           = self.__params_handler.get_param(GavcClientBaseParamsHandler.TOKEN_PARAM)
+
         config = UserConfig.load()
-        config.set_value(GavcClientBaseParamsHandler.SERVER_PARAM,      self.__connection_params_page.server())
-        config.set_value(GavcClientBaseParamsHandler.REPOSITORY_PARAM,  self.__connection_params_page.repository())
-        config.set_value(GavcClientBaseParamsHandler.TOKEN_PARAM,       self.__connection_params_page.token())
+        config.set_value(GavcClientParamsHandler.CACHE_PATH_PARAM,      current_cache_path)
+        config.set_value(GavcClientBaseParamsHandler.SERVER_PARAM,      current_server)
+        config.set_value(GavcClientBaseParamsHandler.REPOSITORY_PARAM,  current_repository)
+        config.set_value(GavcClientBaseParamsHandler.TOKEN_PARAM,       current_token)
         config.store()
 
     def on_cancel(self):
